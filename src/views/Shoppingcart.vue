6df8f9ab7fdf053b17e1f2a1bbdb1dd6fc4cba11
Chouwill <will92068@gmail.com>
2024-02-24 16:53:54 +0800

    購物車畫面修改

--- src/views/Shoppingcart.vue
+++ src/views/Shoppingcart.vue
@@ -5,130 +5,84 @@
       <font-awesome-icon icon="fa-solid fa-xmark" class="cancel" />
     </div>
 
-    <main>
+    <div class="cart_main">
       <table class="cart_table">
-        <tr>
-          <th></th>
-          <th>項目 POKE</th>
-          <th>單價</th>
-          <th>刪除</th>
-        </tr>
-        <tr>
-          <td class="cart_item">
-            <img src="https://picsum.photos/200/150/?random=10" />
-          </td>
-          <td class="cart_item_td">
-            <h5 class="3333">招牌早午餐</h5>
-            <div class="cart_controls">
-              <button><font-awesome-icon icon="fa-solid fa-minus" /></button>
-              <span>1</span>
-              <button><font-awesome-icon icon="fa-solid fa-plus" /></button>
-            </div>
-          </td>
-          <td class="cart_item_td">200</td>
-          <td class="cart_item_td">
-            <button><font-awesome-icon icon="fa-solid fa-trash" /></button>
-          </td>
-        </tr>
-        <tr>
-          <td class="cart_item">
-            <img src="https://picsum.photos/200/150/?random=10" />
-          </td>
-          <td class="cart_item_td">
-            <h5 class="3333">招牌特餐</h5>
-            <div class="cart_controls">
-              <button><font-awesome-icon icon="fa-solid fa-minus" /></button>
-              <span>3</span>
-              <button><font-awesome-icon icon="fa-solid fa-plus" /></button>
-            </div>
-          </td>
-          <td class="cart_item_td">200</td>
-          <td class="cart_item_td">
-            <button><font-awesome-icon icon="fa-solid fa-trash" /></button>
-          </td>
-        </tr>
-        <tr>
-          <td class="cart_item">
-            <img src="https://picsum.photos/200/150/?random=10" />
-          </td>
-          <td class="cart_item_td">
-            <h5 class="3333">招牌早午餐</h5>
-            <div class="cart_controls">
-              <button><font-awesome-icon icon="fa-solid fa-minus" /></button>
-              <span>1</span>
-              <button><font-awesome-icon icon="fa-solid fa-plus" /></button>
-            </div>
-          </td>
-          <td class="cart_item_td">200</td>
-          <td class="cart_item_td">
-            <button><font-awesome-icon icon="fa-solid fa-trash" /></button>
-          </td>
-        </tr>
+        <thead>
+          <tr>
+            <th></th>
+            <th>項目 POKE</th>
+            <th>單價</th>
+            <th width="50">刪除</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td class="cart_item_pic">
+              <img src="https://picsum.photos/200/150/?random=10" />
+            </td>
+            <td class="cart_item_td">
+              <h5>招牌早午餐</h5>
+              <div class="cart_controls">
+                <button class="btn">
+                  <font-awesome-icon icon="fa-solid fa-minus" />
+                </button>
+                <span>1</span>
+                <button class="btn">
+                  <font-awesome-icon icon="fa-solid fa-plus" />
+                </button>
+              </div>
+            </td>
+            <td class="cart_item_price">200</td>
+            <td class="cart_item_trash">
+              <button class="btn btn-trash">
+                <font-awesome-icon icon="fa-solid fa-trash" />
+              </button>
+            </td>
+          </tr>
+        </tbody>
       </table>
       <table class="cart_total">
         <tr>
-          <td>訂單金額</td>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th>1000</th>
+          <th>訂單金額</th>
+          <td>1000</td>
         </tr>
         <tr>
-          <td>運費</td>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th>60</th>
+          <th>運費</th>
+          <td>60</td>
         </tr>
         <tr>
-          <td>總計</td>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th>1060</th>
+          <th>總計</th>
+          <td>1060</td>
         </tr>
       </table>
       <button class="self_checkout">確認結帳</button>
-    </main>
+    </div>
   </div>
 </template>
 
 <script setup></script>
 
 <style lang="scss" scoped>
+@import "../assets/scss/media";
 .cart_wrapper {
-  width: 30%;
-  display: block;
+  width: 750px;
+  max-width: 100%;
   // border: 5px solid red;
   margin: 0 auto;
+  padding: 1rem;
   .cart_header {
-    display: inline-flex;
+    display: flex;
     position: relative;
+    text-align: center;
+    margin-bottom: 1rem;
+    border-bottom: 2px solid #000;
 
     h2 {
+      width: 100%;
       font-size: 40px;
       font-weight: bolder;
       position: relative;
       margin: 15px 0;
-      text-align: center;
-      width: 750px;
-      &::after {
-        border-bottom: 2px solid #000;
-        content: "";
-        left: 0;
-        bottom: -10px;
-        width: 100%;
-        position: absolute;
-      }
     }
     .cancel {
       position: absolute;
@@ -136,7 +90,7 @@
     }
   }
 
-  main {
+  .cart_main {
     width: 95%;
     display: flex;
     flex-direction: column;
@@ -152,29 +106,63 @@
       th,
       td {
         border: 1px solid #000;
-        padding: 10px;
-        text-align: left;
+        text-align: center;
         vertical-align: middle;
-        h5 {
-          margin: 10px 0;
+      }
+      .btn {
+        border: 0;
+        background-color: transparent;
+        &-trash {
+          display: block;
+          background-color: red;
+          color: #fff;
+          width: 100%;
+          height: 100%;
         }
+      }
+      .cart_item_pic {
+        padding: 0;
+        width: 80px;
+        height: 80px;
       }
       .cart_item_td {
         // width: 10vw;
         h5 {
           font-size: 25px;
           font-weight: bolder;
+          margin-top: 0;
+          margin-bottom: 8px;
         }
         span {
           font-size: 25px;
         }
+        @include pad {
+          h5,
+          span {
+            font-size: 18px;
+          }
+        }
+        @include mobile {
+          h5,
+          span {
+            font-size: 14px;
+          }
+        }
+      }
+      .cart_item_price {
+        min-width: 50px;
+      }
+      .cart_item_trash {
+        background-color: red;
       }
       .cart_controls {
         background-color: #17ae62;
-        width: 200px;
         display: flex;
         justify-content: space-around;
         align-items: center;
+        min-width: 100px;
+        max-width: 150px;
+        margin: 0 auto;
       }
     }
     .cart_total {
@@ -190,15 +178,24 @@
       th,
       td {
         padding: 20px;
-        // border: 5px solid gold;
-        text-align: left;
         vertical-align: middle;
+      }
+      th{
+        text-align: left;
+      }
+      td{
+        text-align: right;
       }
     }
   }
   .self_checkout {
+    width: 100%;
     padding: 10px;
     margin: 15px;
+    border: 0;
+    background-color: brown;
+    border-radius: 4px;
+    color: #fff;
   }
 }
 </style>
