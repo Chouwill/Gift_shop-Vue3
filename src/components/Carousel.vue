dbe3fa996ae98ad51e8b59afdab02e10d0c5ae09
Chouwill <will92068@gmail.com>
2024-03-02 16:28:47 +0800

    測試

---
+++ src/components/Carousel.vue
@@ -0,0 +1,50 @@
+<template>
+  <Carousel v-bind="settings" :breakpoints="breakpoints">
+    <Slide v-for="img in imgs" :key="img">
+      <div class="carousel__item">
+        <img :src="img" alt="" />
+      </div>
+    </Slide>
+
+    <template #addons>
+      <Navigation />
+    </template>
+  </Carousel>
+</template>
+
+<script setup>
+import { reactive, defineProps } from "vue";
+import { Carousel, Slide, Navigation } from "vue3-carousel";
+
+defineProps({
+  imgs: {
+    type: {
+      type: Array,
+      required: true,
+    },
+  },
+  settings: {
+    type: {
+      type: Object,
+      default: () => ({
+        itemsToShow: 1,
+        snapAlign: "start",
+      }),
+    },
+  },
+  breakpoints: {
+    type: Object,
+    default: () => ({
+      414: {
+        itemsToShow: 3,
+      },
+      768: {
+        itemsToShow: 4,
+      },
+      1024: {
+        itemsToShow: 7,
+      },
+    }),
+  },
+});
+</script>
