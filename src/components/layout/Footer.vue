a7af1b1f51796b07662c7ab52608094eded1d515
Chouwill <will92068@gmail.com>
2024-04-06 19:11:44 +0800

    註解

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,27 +1,29 @@
 <template>
-  <footer>
+  <footer class="footer">
     <div class="footer_menu">
-      <ul class="footer_column">
-        <h3><a href="#">Shop</a></h3>
-        <li>
-          <a href="#">All</a>
-        </li>
-        <li>
-          <a href="#">Home & Living</a>
-        </li>
-        <li>
-          <a href="#">Accessories</a>
-        </li>
-        <li>
-          <a href="#">Stationery</a>
-        </li>
-        <li>
-          <a href="#">Sale</a>
-        </li>
-        <li>
-          <a href="#">Gift Card</a>
-        </li>
-      </ul>
+      <div class="footer_group">
+        <h3 class="footer_title">Shop</h3>
+        <ul class="footer_column">
+          <li>
+            <a href="#">All</a>
+          </li>
+          <li>
+            <a href="#">Home & Living</a>
+          </li>
+          <li>
+            <a href="#">Accessories</a>
+          </li>
+          <li>
+            <a href="#">Stationery</a>
+          </li>
+          <li>
+            <a href="#">Sale</a>
+          </li>
+          <li>
+            <a href="#">Gift Card</a>
+          </li>
+        </ul>
+      </div>
       <!-- <ul class="footer_column">
         <h3><a href="#">Helpful Links</a></h3>
         <li>
@@ -43,146 +45,146 @@
           <a href="#">Gift Card</a>
         </li>
       </ul> -->
-      <ul class="footer_column">
-        <h3><a href="#">Contact</a></h3>
-        <li>
-          <a href="#">All</a>
-        </li>
-        <li>
-          <a href="#">Home & Living</a>
-        </li>
-        <li>
-          <a href="#">Accessories</a>
-        </li>
-        <li>
-          <a href="#">Stationery</a>
-        </li>
-        <li>
-          <a href="#">Refund Policy</a>
-        </li>
-        <li class="footer_icon">
-          <a href="#"><font-awesome-icon icon="fa-brands fa-instagram" /></a>
-          <a href="#"><font-awesome-icon icon="fa-brands fa-facebook" /></a>
-          <a href="#"><font-awesome-icon icon="fa-brands fa-tiktok" /></a>
-        </li>
-      </ul>
-      <div class="subscribe">
-        <form action="">
-          <h3><a href="#">Subscribe</a></h3>
+      <div class="footer_group">
+        <h3 class="footer_title">Contact</h3>
+        <ul class="footer_column">
           <li>
-            <p>
-              Subscribe to our newsletter and be among the first to<br />
-              hear about new arrivals, events and special offers.
-            </p>
+            <a href="#">All</a>
           </li>
           <li>
-            <label for="">Email*</label>
+            <a href="#">Home & Living</a>
           </li>
           <li>
-            <input type="email" />
-            <button>Subscribe</button>
+            <a href="#">Accessories</a>
           </li>
           <li>
-            <p>© 2035 by JSY. Powered and secured by Wix</p>
-            <!-- 是否要拿掉 -->
+            <a href="#">Stationery</a>
           </li>
+          <li>
+            <a href="#">Refund Policy</a>
+          </li>
+          <li class="footer_icon">
+            <a href="#"><font-awesome-icon icon="fa-brands fa-instagram" /></a>
+            <a href="#"><font-awesome-icon icon="fa-brands fa-facebook" /></a>
+            <a href="#"><font-awesome-icon icon="fa-brands fa-tiktok" /></a>
+          </li>
+        </ul>
+      </div>
+      <div class="footer_group">
+        <h3 class="footer_title">Subscribe</h3>
+        <p class="footer_description">
+          Subscribe to our newsletter and be among the first to<br />
+          hear about new arrivals, events and special offers.
+        </p>
+        <form class="footer_subscribe form">
+          <div class="form-item">
+            <label class="form-label">Email*</label>
+            <input class="form-control" type="email" aria-label="Email" />
+          </div>
+          <div class="form-item">
+            <button class="form-button" type="submit">Subscribe</button>
+          </div>
         </form>
+        <p>© 2035 by JSY. Powered and secured by Wix</p>
       </div>
     </div>
     <div class="footer_logo">
-      <h1>JSY</h1>
+      <span>JSY</span>
     </div>
   </footer>
 </template>
 
-<script setup></script>
+<script setup>
+
+</script>
 
 <style lang="scss" scoped>
-* {
-  list-style: none;
-  box-sizing: border-box;
-  text-decoration: none;
-  color: #000;
-}
-footer {
-  background-color: #F1E4B8;
-  // display: flex;
-  // flex-wrap: wrap;
-  .footer_menu {
+@import "../../assets/scss/media";
+.footer {
+  background-color: #f1e4b8;
+  &_menu {
     display: flex;
-    width: 100%;
-    padding: 50px 0;
-    background-color: #F1E4B8;
     justify-content: space-evenly;
-    align-items: center;
-    .footer_column {
-      h3 {
-        // font-size: 30px;
-        padding: 0 0 20px;
-        font-weight: 700;
-        a {
-          color: #000;
-        }
-      }
-      li {
-        padding: 0 0 8px;
-      }
-      .footer_icon {
-        width: 100%;
-        display: flex;
-        justify-content: space-between;
-        border: 1px solid greenyellow;
-        font-size: 15px;
-      }
-    }
-    .subscribe {
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      // border: 5px solid red;
-      // padding: 0 20px 100px;
-
-      h3 {
-        padding: 0 0 20px;
-        font-weight: 700;
-        a {
-          color: #000;
-        }
-      }
-      li > p{
-        color: red;
-        padding: 0 0 10px;
-      }
-      li{
-        padding: 0 0 10px;
-        input{
-          padding: 5px 10px;
-          border-radius: 100px;
-          border: 3px solid red;
-          outline: none;
-        }
-        button{
-          padding: 10px 25px;
-          margin-left: 10px;
-          background: #000;
-          color: white;
-          border-radius: 100px;
-          font-weight: 700;
-          &:hover{
-            background-color: #F1E4B8;
-            color: #000;
-          }
-        }
-      }
+    width: 100%;
+    max-width: 1200px;
+    margin: 0 auto;
+    padding: 50px 24px;
+    gap: 20px;
+    flex-wrap: wrap;
+  }
+  &_title {
+    font-weight: bold;
+    margin-bottom: 1rem;
+  }
+  &_group {}
+  &_column {
+    font-weight: normal;
+    li {
+      margin-bottom: 0.8rem;
     }
   }
-  .footer_logo {
-    background-color: #F1E4B8;
-    h1 {
-      font-size: 120px;
-      text-align: center;
-      padding: 50px 0;
+  &_icon {
+    display: flex;
+    gap: 0.8rem;
+    font-size: 1rem;
+  }
+  &_description {
+    margin-bottom: 1rem;
+  }
+  &_subscribe {
+    width: 100%;
+    display: flex;
+    align-items: end;
+    gap: 8px;
+    margin-bottom: 2rem;
+    .form-item:first-of-type {
+      flex: 1;
+    }
+  }
+  &_logo {
+    text-align: center;
+    padding: 0 24px 50px;
+    span {
+      font-size: 100px;
+      font-weight: 700;
     }
   }
 }
+
+.form {
+  &-item {
+    margin-bottom: 8px;
+  }
+  &-label {
+    display: block;
+    margin-bottom: 4px;
+  }
+  &-control {
+    width: 100%;
+    height: 35px;
+    border-radius: 20px;
+    border: 2px solid #000;
+    padding: 3px 8px;
+    background-color: transparent;
+    &:focus {
+      outline: none;
+    }
+  }
+  &-button {
+    width: 100%;
+    height: 35px;
+    border-radius: 20px;
+    border: 2px solid #000;
+    padding: 3px 16px;
+    background-color: #000;
+    color: #fff;
+    cursor: pointer;
+    transition: all 300ms ease-in-out;
+    &:hover {
+      background-color: transparent;
+      color: #000;
+    }
+  }
+}
+
 </style>
